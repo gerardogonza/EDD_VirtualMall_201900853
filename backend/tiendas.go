ea96258b135420c402edcc9d26fb3a2a94b4da2e
gerardogonza <gerardocontreras@users.noreply.github.com>
2021-03-17 21:35:06 -0600

    Initial commit

---
+++ backend/tiendas.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+
+var path = "grafo.dot"
+var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+var path1 = "documento.json"
+var file1, err1 = os.OpenFile(path1, os.O_RDWR, 0644)
+var tiendas_grafo, conexiones_grafo,datos_tiendas string
+var nodo, valor int
+
+type indice struct {
+	Datos []indicess
+}
+type indicess struct {
+	Indice        string
+	Departamentos []departamentos
+}
+type departamentos struct {
+	Nombre  string
+	Tiendas []tienda
+}
+type tienda struct {
+	Nombre       string
+	Descripcion  string
+	Contacto     string
+	Calificacion int
+}
+
+var indices indice
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", home)
+	router.HandleFunc("/cargartienda", cargartienda).Methods("GET")
+	router.HandleFunc("/getArreglo", getArreglo ).Methods("GET")
+	router.HandleFunc("/TiendaEspecifica", TiendaEspecifica).Methods("POST")
+	router.HandleFunc("/id/{numero}",busquedaporPosicion).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Servidor Funcionando :D")
+}
+
+func cargartienda(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, &indices)
+	if err != nil {
+		log.Fatal("Error")
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(indices)
+}
+func busquedaporPosicion(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["numero"])
+	if err != nil {
+		return
+	}
+
+	valor=taskID
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(datos_tiendas)
+
+
+}
+
+func getArreglo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode("<img src=\"imagengrafo.jpg\">")
+
+}
+func TiendaEspecifica(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode("<img src=\"dinosaur.jpg\">")
+
+}
+
+
