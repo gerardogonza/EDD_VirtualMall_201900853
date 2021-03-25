68ca1bdfc7d7b2e9648aa6671e6fe62aa08a960a
gerardogonza <irjecogerardo@gmail.com>
2021-03-25 01:36:38 -0600

    carrito aun no recibe datos

--- src/app/componentes/inventario/inventario.component.ts
+++ src/app/componentes/inventario/inventario.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
+import { CarritoService } from "../../services/carrito.service";
+import { Producto } from 'src/app/interfaces/models';
+import { dashCaseToCamelCase, partitionArray } from '@angular/compiler/src/util';
 @Component({
   selector: 'app-inventario',
   templateUrl: './inventario.component.html',
@@ -8,9 +11,10 @@ import { HttpClient } from "@angular/common/http";
 })
 export class InventarioComponent implements OnInit {
 
-  constructor(private activedRoute: ActivatedRoute,private http: HttpClient) { }
+  constructor(private activedRoute: ActivatedRoute,private http: HttpClient, public carritoServices:CarritoService) { }
   conversion
-  tiendas:any;
+  tiendas:Producto[]=[];
+
   ngOnInit(): void {
     const params =this.activedRoute.snapshot.params;
     if(params.nombre){
@@ -18,9 +22,21 @@ export class InventarioComponent implements OnInit {
       .subscribe(data => {
         this.conversion=data
         this.tiendas=this.conversion;
-        console.log('Tiendas',this.tiendas)
+        // console.log('Tiendas',this.tiendas)
       });
     }
   }
+
+addCarrito(index:number){
+this.carritoServices.addProducto(this.conversion[index]);
+}
+getIndex(Codigo:number):number{
+  for(var i = 0; i < this.conversion.length; i++){
+    if(this.conversion[i].Codigo==Codigo){
+      return i
+    }
+  }
+  return 0 
+}
 
 }
