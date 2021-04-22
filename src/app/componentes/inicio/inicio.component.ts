57270b8a914d6b1aaa7bde68d4337dbbd485a5eb
gerardogonza <irjecogerardo@gmail.com>
2021-04-21 19:47:43 -0600

    login terminado Front y back

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http:HttpClient) { }
+  conversion
+  usuarios:any=[];
   ngOnInit(): void {
+    this.http.get('http://localhost:3000/mostrarusuarios')
+    .subscribe(data => {
+      this.conversion=data
+      this.usuarios=this.conversion;
+      console.log('Usuarios',this.usuarios)
+    });
+  }
+  Dpi:string; //Admin
+  Password:string;
+  DpiUser:string; //User
+  PasswordUser:string;
+  url = `http://localhost:3000/`;
+  
+  alogin(){
+    
+    if(this.Dpi=="1234567890101"&& this.Password=="1234"){
+      console.log("Bienvenido Admin");
+      window.location.href = 'http://localhost:4200/cargarjson';
+    }
+  for(var i = 0; i < this.conversion.length; i++){
+    if(this.conversion[i].Dpi==this.Dpi&&this.conversion[i].Password==this.Password&&this.conversion[i].Cuenta=="Admin"){
+     
+      window.location.href = 'http://localhost:4200/cargarjson';
+    }
   }
 
+  alert('Datos Erroneos');
+  return 0 
+}
+ulogin(){
+  
+  for(var i = 0; i < this.conversion.length; i++){
+    if(this.conversion[i].Dpi==this.DpiUser&&this.conversion[i].PasswordUser==this.Password&&this.conversion[i].Cuenta=="Usuario"){
+      window.location.href = 'http://localhost:4200/modificaciones';
+    }
+  }
+  alert('Datos Erroneos');
+  return 0 
+}
 }
